747847c6d263dad692e99b21ca0697b0d76ca580
natecordova <natecordovaortiz@hotmail.com>
2017-07-07 06:05:31 +0000

    Pruebas Unitarias

--- pruebatravis.php
+++ pruebatravis.php
@@ -1,1 +1,14 @@
-prueba dos travis-- liss
+<?php
+use PHPUnit\Framework\TestCase;
+
+class ExpectedErrorTest extends TestCase
+{
+    /**
+     * @expectedException PHPUnit_Framework_Error
+     */
+    public function testFailingInclude()
+    {
+        include 'not_existing_file.php';  #Pruebas unitarias --nate
+    }
+}
+?>
