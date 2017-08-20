26eb38541a0cd29779ae30cd95f7338583000fb2
lisscontreras <lisscb1104@gmail.com>
2017-08-20 13:20:08 -0500

    rol php

---
+++ pages/rol.php
@@ -0,0 +1,27 @@
+<?php
+
+class rol
+{
+    private $idrol;
+    private $nombre;
+    
+     function __construct($idrol, $nombre) {
+       $this->iddemo = $idrol;
+       $this->nombre = $nombre;
+     }
+    
+     function setIdRol($idrol){
+       $this->idrol = $idrol;
+     } 
+     function getIdRol(){
+       return $this->idRol;
+     } 
+     function setNombre($nombre){
+       $this->nombre = $nombre;
+     } 
+     function getNombre(){
+       return $this->nombre;
+     } 
+}
+
+?> 
