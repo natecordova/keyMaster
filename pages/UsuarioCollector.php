b3e6886f1d87c4618b56caeb0a119f389a43e18b
natecordova <natecordovaortiz@hotmail.com>
2017-08-28 23:57:26 +0000

    Administrador Control Panel Usuario

--- pages/UsuarioCollector.php
+++ pages/UsuarioCollector.php
@@ -1,39 +1,47 @@
 <?php
 
-class Usuario
+include_once('Usuario.php');
+include_once('Collector.php');
+
+class UsuarioCollector extends Collector
 {
-    private $id_usuario;
-    private $nombre;
-    private $contrasena;
+  
+  function showDemos() {
+    $rows = self::$db->getRows("SELECT * FROM usuario ");        
+
+    $arrayDemo= array();        
+    foreach ($rows as $c){
+      $aux = new Usuario($c{'id'},$c{'nombre'},$c{'contrasena'});
+      array_push($arrayDemo, $aux);
+    }
+    return $arrayDemo;        
+  }
+function showDemo($id) {
+    $rows = self::$db->getRows("SELECT * FROM usuario where id= ? ", array ("{$id}"));        
+$ObjDemo= new Usuario($rows[0]{'id'},$rows[0]{'nombre'},$rows[0]{'contrasena'});
     
-    
-     function __construct($id_usuario, $nombre, $contrasena) {
-       $this->id_usuario = $id_usuario;
-       $this->nombre = $nombre;
-        $this->contrasena = $contrasena;
-     }
-    
-     function setIdUsuario($id_usuario){
-       $this->id_usuario = $id_usuario;
-     } 
-     function getIdUsuario(){
-       return $this->id_usuario;
-     } 
-     function setNombre($nombre){
-       $this->nombre = $nombre;
-     } 
-     function getNombre(){
-       return $this->nombre;
-     } 
-     function setContrasena($contrasena){
-       $this->contrasena = $contrasena;
-     } 
-     function getContrasena(){
-       return $this->contrasena;
-     } 
-     
-     
-     
+    return $ObjDemo;        
+  }
+function UpdateDemo($id,$nombre,$contrasena) {
+    $insertrow = self::$db->updateRow("UPDATE public.usuario SET nombre = ?, contrasena = ? where id= ? ", array ("{$nombre}","{$contrasena}",$id));
+
+}
+function deleteDemo($id) {
+    $deleterow = self::$db->deleteRow("DELETE FROM public.usuario where id= ? ", array ("{$id}"));
+
 }
 
-?> 
+function createDemo($nombre,$contrasena){
+  
+
+    $insertrow = self::$db->insertRow("INSERT INTO public.usuario(nombre, contrasena) VALUES (?,?)", array ("{$nombre}","{$contrasena}"));             
+  
+
+}
+
+
+
+
+}
+?>
+
