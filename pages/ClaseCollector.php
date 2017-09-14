1b477e07548818734b4e8e7f27121ae9c4c80c6b
willmont21 <willmont66@gmail.com>
2017-09-14 17:03:11 -0500

    Update ClaseCollector.php

--- pages/ClaseCollector.php
+++ pages/ClaseCollector.php
@@ -8,22 +8,22 @@ class ClaseCollector extends Collector
   
   function showDemos() {
     $rows = self::$db->getRows("SELECT * FROM clase ");        
-    
+     
     $arrayDemo= array();        
     foreach ($rows as $c){
-      $aux = new Demo($c{'id_clase'});
+      $aux = new Clases($c{'id_clase'}, $c{'fk_id_profesor'});
       array_push($arrayDemo, $aux);
     }
     return $arrayDemo;        
   }
 function showDemo($id) {
     $rows = self::$db->getRows("SELECT * FROM clase where id_clase= ? ", array ("{$id}"));        
-$ObjDemo= new Clases($rows[0]{'id_clase'},$rows[0]{'profesor'});
+    $ObjDemo= new Clases($rows[0]{'id_clase'},$rows[0]{'fk_id_profesor'});
     
     return $ObjDemo;        
   }
 function UpdateDemo($id,$profesor) {
-    $insertrow = self::$db->updateRow("UPDATE clase SET profesor = ? where id_clase= ? ", array ("{$profesor}",$id));
+    $insertrow = self::$db->updateRow("UPDATE clase SET fk_id_profesor = ? where id_clase= ? ", array ("{$profesor}",$id));
 
 }
 
