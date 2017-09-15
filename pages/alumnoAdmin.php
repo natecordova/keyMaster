175a13ee906dd3b9cdf340e5a83637f474b40a32
willmont21 <willmont66@gmail.com>
2017-09-15 10:57:51 -0500

    Update alumnoAdmin.php

--- pages/alumnoAdmin.php
+++ pages/alumnoAdmin.php
@@ -203,21 +203,10 @@ foreach ($DemoCollectorObj->showDemos() as $c){
               </div>
               <div class="panel-footer">
                 <div class="row">
-                  <div class="col col-xs-4">Keymaster
+                  <div class="col col-xs-11">Keymaster
                   </div>
-                  <!--<div class="col col-xs-8">
-                    <ul class="pagination hidden-xs pull-right">
-                      <li><a href="#">1</a></li>
-                      <li><a href="#">2</a></li>
-                      <li><a href="#">3</a></li>
-                      <li><a href="#">4</a></li>
-                      <li><a href="#">5</a></li>
-                    </ul>
-                    <ul class="pagination visible-xs pull-right">
-                        <li><a href="#">«</a></li>
-                        <li><a href="#">»</a></li>
-                    </ul>
-                  </div> -->
+		<div class="col col-xs-1"><a href="vistaAdmin.php">Volver</a>
+                  </div>	
                 </div>
               </div>
             </div>
