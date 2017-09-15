581e5256bcfe17c9b71683629dcc6b7be8adb256
willmont21 <willmont66@gmail.com>
2017-09-14 23:52:18 -0500

    Update curso.php

--- pages/curso.php
+++ pages/curso.php
@@ -4,10 +4,14 @@ class curso
 {
     private $idcurso;
     private $detalle;
+    private $id_profesor_fk;
+    private $id_niveles_fk;
     
-     function __construct($idcurso, $detalle) {
+     function __construct($idcurso, $detalle, $profesor, $niveles) {
        $this->idcurso = $idcurso;
        $this->detalle = $detalle;
+       $this->id_profesor_fk = $profesor;
+       $this->id_niveles_fk = $niveles;
      }
     
      function setIdCurso($idcurso){
@@ -21,7 +25,19 @@ class curso
      } 
      function getDetalle(){
        return $this->detalle;
+     }
+     function setIdProfesor($idprofesor){
+       $this->id_profesor_fk = $idprofesor;
      } 
+     function getIdProfesor(){
+       return $this->id_profesor_fk;
+     } 
+     function setIdNiveles($niveles){
+       $this->id_niveles_fk = $niveles;
+     } 
+     function getIdNiveles(){
+       return $this->id_niveles_fk;
+     }
 }
 
 ?> 
