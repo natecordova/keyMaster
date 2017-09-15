83a5d7df9ab08f9e921dcc3b07061ed746ed4cf5
willmont21 <willmont66@gmail.com>
2017-09-15 11:36:52 -0500

    Update PersonaAdmin.php

--- pages/PersonaAdmin.php
+++ pages/PersonaAdmin.php
@@ -24,7 +24,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>                        
               </button>
-              <a class="navbar-brand" href="admin.php">KeymasterAdmin</a>
+              <a class="navbar-brand" href="vistaAdmin.php">KeymasterAdmin</a>
             </div>
             <div class="collapse navbar-collapse" id="myNavbar">
               <ul class="nav navbar-nav navbar-right">
